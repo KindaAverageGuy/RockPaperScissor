f21c328de7d7ff2dde33c929fa42a801b3d4a8a4
KindaAverageGuy <KindaAverageGuy@protonmail.com>
2019-10-06 12:00:23 -0400

    Added numeric Selection to assign choice to number

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+// Function will be called by computer
 let computerPlay = function(){
-    return (Math.floor(Math.random() * 3) >= 2) ? "Rock" :
-        (Math.floor(Math.random() * 3) >= 1) ? "Scissor" : "Paper";   
+    return (Math.floor(Math.random() * 3) >= 2) ? 'rock' :
+        (Math.floor(Math.random() * 3) >= 1) ? 'scissor' : 'paper';   
 }
-console.log(computerPlay());
+
+// This function will compare and declare the winner using other functions
+let result = function(playerSelection, computerSelection) {
+    let revisedPlayerSelection = playerSelection.toLowerCase();
+    let revisedNumericSelection = ''
+
+    if (revisedPlayerSelection === 'rock')
+        revisedNumericSelection = 3;
+    else if (revisedPlayerSelection === 'scissor')
+        revisedNumericSelection = 2;
+    else
+        revisedNumericSelection = 1;
+
+
+return revisedNumericSelection;
+}
+
+console.log(result('pApEr', computerPlay()));
